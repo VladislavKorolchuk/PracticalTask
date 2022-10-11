7b895051e1b9596548d98ffdf974d013f82273cf
93294 <9329496@mail.ru>
2022-10-11 16:34:34 +0300

    Практическое задание к экзаменационной работе.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] strings = {"Январь", "Февраль", "Март", "Январь"};
+        System.out.println(Arrays.toString(strings));
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[i] != "") {
+                    if (strings[i].equals(strings[j])) {
+                        strings[j] = "";
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(strings));
+        String result = String.join(" ", strings);
+        System.out.println("Объединение строк массива - " + result);
     }
 }
